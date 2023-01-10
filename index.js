8c4b4963bcec0b39f474e2a98a28f2a9cd5f0857
damilolaakinbi <akinbidamilola074@gmail.com>
2023-01-10 13:28:22 +0100

    Completed

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2;
+
+const random = Math.ceil(Math.random() * 10);
+
+let num3 = 11;
+let num4 = 7;
+const mod = num3 % num4;
+
+const max = Math.max(-10, 5, 15, 20);
